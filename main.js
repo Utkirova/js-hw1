71a0bd79474e89cc531faba18dbea3ec82d525e5
Mohinur <15@02>
2022-12-24 16:38:06 +0500

    save

---
+++ main.js
@@ -0,0 +1,16 @@
+var firstname = prompt('Введите свое имя')
+console.log('Я ' + firstname);
+var age = prompt('Ваш возраст')
+console.log('Мне ' + age);
+var plus = prompt('Решите сложение 430 + 670 = ?')
+console.log('1) 430 + 670 = 1100,  Ваш ответ: ' + plus);
+var minus = prompt('Решите вычитание 40 - 56 = ?')
+console.log('2) 40 - 56 = -16,  Ваш ответ: ' + minus);
+var product = prompt('Найдите произведение 3 * 10^4 = ?')
+console.log('3) 3 * 10^4 = 30000,  Ваш ответ: ' + product);
+var division = prompt('Решите деление 0 / 350 = ?')
+console.log('4) 0 / 350 = 0,  Ваш ответ: ' + division);
+var remainder = prompt('Найдите остаток 27 % 6 = ?')
+console.log('5) 27 % 6 = 3,  Ваш ответ: ' + remainder);
+var  end = prompt('Oткройте консоль для просмотра')
+
